b8f11e6a954213021afb5edf7b0e54535d1921f2
Menersar <menersar@menersar.com>
2023-05-17 03:33:01 +0200

    clean up code

--- buildAllExtensions.js
+++ buildAllExtensions.js
@@ -1,15 +1,10 @@
 const process = require('process');
-const { execSync } = require('child_process')
+// const { execSync } = require('child_process')
 const { exec } = require('child_process');
 var npmAddScript = require('npm-add-script')
 
 
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ZipWebpackPlugin = require('zip-webpack-plugin');
-
-const info = require('./info.json');
-
 
 const fs = require('fs');
 
@@ -18,34 +13,34 @@ const extensionFolder = 'extensions';
 const pathToExtensionFolder = path.resolve(__dirname, extensionFolder);
 var extensionName = '';
 var extensionToBuild = '';
-var extensionInfo = '';
 
 var implementedExtensionFolderNames = fs.readdirSync(pathToExtensionFolder);
 
 
-const executeCommand = function (cmd, successCallback, errorCallback) {
+const executeCommand = function (cmd, successCallback, errorCallback, extensionName) {
+    console.log('Building Extension: ', extensionName);
     exec(cmd, (error, stdout, stderr) => {
         if (error) {
-            // console.log(`error: ${error.message}`);
+            console.log(`error: ${error.message}`);
             if (errorCallback) {
                 // errorCallback(error.message);
                 console.log(error.message);
             }
-            return;
+            // return;
         }
         if (stderr) {
-            //console.log(`stderr: ${stderr}`);
+            console.log(`stderr: ${stderr}`);
             if (errorCallback) {
                 // errorCallback(stderr);
-               console.log(stderr);
+                // console.log(stderr);
             }
-            return;
+            // return;
         }
-        //console.log(`stdout: ${stdout}`);
+        console.log(`stdout: ${stdout}`);
         if (successCallback) {
             // successCallback(stdout);
-            console.log(stdout);
-
+            // console.log(stdout);
+            console.log('Built Extension: ', extensionName);
         }
     });
 };
@@ -53,39 +48,15 @@ const executeCommand = function (cmd, successCallback, errorCallback) {
 
 // console.log(`${implementedExtensionFolderNames[0]}`);
 
-
-
 implementedExtensionFolderNames.forEach((val, index) => {
-
-    // console.log(`${index}: ${val}`);
-
-    // if (index >= 2) {
 
     extensionName = val;
     extensionToBuild = extensionFolder + '/' + extensionName + '/';
 
     // npmAddScript({ key: "build:" + extensionName, value: "rimraf ./" + extensionToBuild + "build && mkdirp " + extensionToBuild + "build && rimraf ./" + extensionToBuild + "dist && mkdirp " + extensionToBuild + "dist && node ./custom-webpack-config.js " + extensionName + " && webpack --env extension=" + extensionName, force: true });
     npmAddScript({ key: "build:" + extensionName, value: "rimraf ./" + extensionToBuild + "build && mkdirp " + extensionToBuild + "build && rimraf ./" + extensionToBuild + "dist && mkdirp " + extensionToBuild + "dist && webpack --env extensionName=" + extensionName, force: true });
-    // npmAddScript({ key: "build:" + extensionName, value: "rimraf ./" + extensionToBuild + "build && mkdirp " + extensionToBuild + "build && rimraf ./" + extensionToBuild + "dist && mkdirp " + extensionToBuild + "dist && node custom-webpack-config.js " + extensionName, force: true });
 
-    // console.log("yarn run build:" + extensionName);
-
-
-    // exec("yarn run build:" + extensionName);
-
-
-
-    // exec(cmd, (error, stdout, stderr))
-
-
-
-    executeCommand("yarn run build:" + extensionName, "sik", "error");
-
-
-
-
-
-    // }
+    executeCommand("yarn run build:" + extensionName, "success", "error", extensionName);
 });
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,144 +2,29 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ZipWebpackPlugin = require('zip-webpack-plugin');
 
-// const { changeConfig } = require('./custom-webpack-config.js');
-// const { customConfig } = require('./custom-webpack-config.js');
-const info = require('./info.json');
-
-
-
 // const info = require('./info.json');
 
 const process = require('process');
-// const fs = require('fs');
 
-// const extensionFolder = 'extensions';
-// var extensionToBuild = "";
-// var extensionInfo = "";
+const extensionsFolderName = 'extensions';
 
-// extensionInfo = require('./info.json');
-
-// console.log("hierrrrrrrrrrrrrrrrr: " + info);
-
-
-const extensionFolder = 'extensions';
-
-
-
-// let config = {
-//     bail: true,
-//     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-//     entry: './index.js',
-//     output: {
-//         filename: 'main.js',
-//         path: path.resolve(__dirname, 'build'),
-//         module: true,
-//         library: {
-//             type: 'commonjs2'
-//         }
-//     },
-//     experiments: {
-//         outputModule: true
-//     },
-//     externals: {
-//         'scratch-extension': 'ScratchExtension'
-//     },
-//     externalsType: 'global',
-//     plugins: [
-//         new CopyWebpackPlugin({
-//             patterns: [{
-//                 from: path.join(__dirname, 'locales'),
-//                 to: path.join(__dirname, 'build/locales')
-//             }, {
-//                 from: path.join(__dirname, 'assets'),
-//                 to: path.join(__dirname, 'build/assets')
-//             }, {
-//                 from: path.join(__dirname, 'info.json'),
-//                 to: path.join(__dirname, 'build/info.json')
-//             }]
-//         }),
-//         new ZipWebpackPlugin({
-//             path: path.join(__dirname, 'dist'),
-//             filename: `${info.id}@${info.version}`,
-//             extension: 'skx'
-//         })
-//     ]
-// };
-
-
-
-
-
-
-
-
-// module.exports = config;
-
-
-
-
-
-// changeConfig(config);
-
-
-
-
-// let customConf = {};
-
-// changeConfig(config);
-
-// console.log("Extension Info:____________");
-// console.log(config);
-
-
-// console.log(customConf.someProperty);    // 2
-// console.log(configuration);    // 2
-
-
-
-
-
-
-
-// module.exports = config;
-
-
-
-
-
-
-
-
-
-
-// config.entry = customConfig.entry;
-// config.output.path = customConfig.output.path;
-
-
-// config.plugins[0].patterns[0].from = customConfig.plugins[0].patterns[0].from
-// config.plugins[0].patterns[0].to = customConfig.plugins[0].patterns[0].to;
-
-// config.plugins[0].patterns[1].from = customConfig.plugins[0].patterns[1].from
-// config.plugins[0].patterns[1].to = customConfig.plugins[0].patterns[1].to;
-
-// config.plugins[0].patterns[2].from = customConfig.plugins[0].patterns[2].from
-// config.plugins[0].patterns[2].to = customConfig.plugins[0].patterns[2].to;
-
-
-// config.plugins[1].path = customConfig.plugins[1].path;
-// config.plugins[1].filename = customConfig.plugins[1].filename;
-// config.plugins[1].extension = customConfig.plugins[1].extension;
-
-
+let extensionLocation = '';
+let extensionInfo = '';
+let extensionName = '';
 
 
 module.exports = (env) => {
+    // Using Webpack Environment Variables (https://webpack.js.org/guides/environment-variables/):
+    // To create webpack configs for building specific extensions
     // Use env.<YOUR VARIABLE> here:
-    console.log('Extension: ', env.extensionName); // 'local'
-    // console.log('Production: ', env.production); // true
+    // console.log('Extension: ', env.extensionName);
 
-    var extensionLocation = extensionFolder + '/' + env.extensionName + '/';
-    var extensionInfo = require('./' + extensionLocation + 'info.json');
+    if (env.extensionName) {
+        extensionName = env.extensionName;
+        extensionLocation = extensionsFolderName + '/' + extensionName + '/';
+    }
+
+    extensionInfo = require('./' + extensionLocation + 'info.json');
 
 
     return {
